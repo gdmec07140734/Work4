3fa32edd82e4ebae34bb5763c27be787af8aa095
gdmec07140734 <2295883498@qq.com>
2015-10-30 14:21:27 +0800

    Initial commit

---
+++ app/src/main/java/com/gdmec07140734/android/work4/ContentTable.java
@@ -0,0 +1,34 @@
+package com.gdmec07140734.android.work4;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+
+/**
+ * Created by JackpanYam .
+ */
+public class ContentTable {
+    private  final static String TABLENAME="contactsTable";
+    private  MyDb db;
+    public ContentTable(Context context){
+        db =new MyDb(context);
+        if(!db.isTableExit(TABLENAME)){
+            String createtableSql = "create table if not exists"+TABLENAME+"(id_DB integer,"+"primary key AUTOINCREMENT,"+
+                    MyUser.getName() +"VARCHAR,"+
+                    MyUser.getMoblie()+"VARCHAR,"+
+                    MyUser.getQq()+"VARCHAR,"+
+                    MyUser.getDdanwei()+"VARCHAR,"+
+                    MyUser.getAddress()+"VARCHAR,";
+            db.CreatTabel(createtableSql);
+        }
+    }
+    public boolean addData(MyUser user){
+        ContentValues values = new ContentValues();
+        values.put(MyUser.getName(),user.getName());
+        values.put(MyUser.getMoblie(),user.getMoblie());
+        values.put(MyUser.getQq(),user.getQq());
+        values.put(MyUser.getDdanwei(),user.getDdanwei());
+        values.put(MyUser.getAddress(),user.getAddress());
+        return db.save(TABLENAME,values);
+    }
+}
